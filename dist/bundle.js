9becab71e623e983259418784237ee674457576f
Sameer Misbah <sameermisbah18@gmail.com>
2024-04-19 10:40:15 +0500

    Initial commit

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.tsx":
+/*!***********************!*\
+  !*** ./src/index.tsx ***!
+  \***********************/
+/***/ (function() {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: [BABEL]: Cannot find module '@babel/traverse'\\nRequire stack:\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\index.js\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\loader-runner\\\\lib\\\\loadLoader.js\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\webpack\\\\lib\\\\NormalModuleFactory.js\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\webpack\\\\lib\\\\Compiler.js\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\webpack\\\\lib\\\\webpack.js\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\webpack\\\\lib\\\\index.js\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\webpack-cli\\\\lib\\\\webpack-cli.js\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\webpack-cli\\\\lib\\\\bootstrap.js\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\webpack-cli\\\\bin\\\\cli.js\\n- C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\webpack\\\\bin\\\\webpack.js (While processing: C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\@babel\\\\preset-env\\\\lib\\\\index.js)\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:880:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:725:27)\\n    at Module.require (internal/modules/cjs/loader.js:952:19)\\n    at require (internal/modules/cjs/helpers.js:88:18)\\n    at _traverse (C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\index.js:207:16)\\n    at Object.get [as traverse] (C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\index.js:178:12)\\n    at Object.<anonymous> (C:\\\\Users\\\\Sameer\\\\Desktop\\\\MS (CS)\\\\Thesis Material\\\\Projects\\\\web3-citizens-app\\\\node_modules\\\\@babel\\\\plugin-proposal-async-generator-functions\\\\node_modules\\\\@babel\\\\helper-remap-async-to-generator\\\\lib\\\\index.js:26:28)\\n    at Module._compile (internal/modules/cjs/loader.js:1063:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1092:10)\\n    at Module.load (internal/modules/cjs/loader.js:928:32)\");\n\n//# sourceURL=webpack://web3-citizens-app/./src/index.tsx?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.tsx"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/api/index.ts
@@ -0,0 +1,76 @@
+import Web3 from 'web3';
+import { v4 as uuidv4 } from 'uuid';
+
+import { CONTACT_ABI, CONTACT_ADDRESS } from '../constants';
+import { getCitizensIdsToSearch } from './utils';
+import { Citizen } from '../types';
+
+const NOT_FOUND = 'City not found';
+const WRONG_FORMAT_MESSAGE = 'Decoded with wrong format.'
+
+const provider = 
+    (window as any).ethereum || 
+    (window as any).web3?.currentProvider;
+
+const web3 = new Web3(provider);
+const contract = new web3.eth.Contract(CONTACT_ABI as any, CONTACT_ADDRESS);
+
+export const citizensAPI = {
+    getCitizensCount: async (): Promise<number> => {
+        const events =  await contract.getPastEvents('Citizen', {
+            fromBlock: 0,
+            toBlock: 'latest'
+        });
+
+        return events.length;
+    },
+
+    fetchCitizens: async (page: number, limit: number, count: number): Promise<Citizen[]> => {
+        const citizenIds = getCitizensIdsToSearch(page, limit, count);
+        
+        const events =  await contract.getPastEvents('Citizen', {
+            filter: {id: citizenIds},
+            fromBlock: 0,
+            toBlock: 'latest'
+        });
+
+        const transactionsData = events.map(async ({ transactionHash, returnValues: { id, age, name } }) => {
+            try {
+                const { input }  = await web3.eth.getTransaction(transactionHash);
+                const parametersTypes = [
+                    { type: 'uint256', name: 'age' },
+                    { type: 'string', name: 'city' },
+                    { type: 'string', name: 'name' },
+                ];
+                const { city } = web3.eth.abi.decodeParameters(parametersTypes, input.slice(10));
+                const isCityHex = web3.utils.isHex(city);
+
+                if (isCityHex) throw new Error(WRONG_FORMAT_MESSAGE);
+
+                return { id, age, name, city };
+            } catch(error) {
+                console.error(error);
+
+                return { id, age, name, city: NOT_FOUND };
+            }
+        })
+
+        return await Promise.all(transactionsData);
+    },
+
+    fetchNote: async (id: string): Promise<string> => {
+        return await contract.methods.getNoteByCitizenId(id).call();
+    },
+
+    addNewCitizen: async (citizen: any): Promise<Citizen> => {
+        const { age, name, city, note } = citizen;
+        const { events } =  await contract.methods
+            .addCitizen(age, city, name, note)
+            .send({ 
+                from: (window as any).ethereum.selectedAddress 
+            });
+        const id = events?.Citizen?.returnValues?.id || uuidv4();
+
+        return { id, age, name, city };
+    }
+};
